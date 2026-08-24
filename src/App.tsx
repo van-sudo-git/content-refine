76f5b2bc04a125c7afd51c3a94d4681280d06dc0
Evaan Ahlawat <evaanahlawat@gmail.com>
2026-08-24 13:36:01 -0700

    feat: add multi-school gallery routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,10 @@
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
 import { Toaster as Sonner } from "@/components/ui/sonner";
 import { Toaster } from "@/components/ui/toaster";
 import { TooltipProvider } from "@/components/ui/tooltip";
 import Index from "./pages/Index.tsx";
+import Galleries from "./pages/Galleries.tsx";
 import Gallery from "./pages/Gallery.tsx";
 import ProfilePage from "./pages/ProfilePage.tsx";
 import About from "./pages/About.tsx";
@@ -27,8 +28,15 @@ const App = () => (
         <PageViewTracker />
         <Routes>
           <Route path="/" element={<Index />} />
-          <Route path="/gallery" element={<Gallery />} />
+
+          {/* Old gallery links now go to the chapter directory. */}
+          <Route path="/gallery" element={<Navigate to="/galleries" replace />} />
+          <Route path="/galleries" element={<Galleries />} />
+          <Route path="/galleries/:schoolSlug" element={<Gallery />} />
+
+          {/* Individual profile URLs stay unchanged so QR codes keep working. */}
           <Route path="/gallery/:slug" element={<ProfilePage />} />
+
           <Route path="/about" element={<About />} />
           <Route path="/media" element={<Media />} />
           <Route path="/nominate" element={<Nominate />} />
